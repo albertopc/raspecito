a798e22f28aa084fc874f6a28954f811a7adb5a2
albertopc <pi@raspecito.(none)>
2015-04-12 15:53:03 +0200

    Si opción --display-only no se publica, sólo se pinta en el display

--- wstation.py
+++ wstation.py
@@ -2,8 +2,9 @@
 # -*- coding: utf-8 -*-
 
 import os
-import sys
+import sys, getopt
 import time
+import datetime
 
 import config
 
@@ -20,6 +21,10 @@ if __name__ == "__main__":
 	thingspeak_key=config.thingspeak_wstation_key
 	
 	try:
+		ahora=datetime.datetime.now().strftime('%d/%m/%Y %H:%M')
+		print(ahora)
+		display_text(0,0, ahora)
+		
 		(Humi,InTemp)=readHumidity()
 		print("DHT22 Humidity:\t{0} %".format(Humi))
 		print("DHT22 Temp:\t{0} C".format(InTemp))
@@ -27,23 +32,23 @@ if __name__ == "__main__":
 		OutTemp=readTemperature()[0] # Take value in celsius only
 		print("DS18B20 Temp:\t{0} C".format(OutTemp))
 
-		display_text(2,2, "{:.2f} C".format(OutTemp)+" / {0} %".format(Humi))
+		display_text(0,12, "{:.2f} C".format(OutTemp))
+		display_text(65,12, "{0} %".format(Humi))
 		
 		(Pressure, InTemp2, Altitude) = readPressure()
 		print("GY65 Pressure:\t{0} hPa".format(Pressure))
 		print("GY65 Temp:\t{0} C".format(InTemp2))
 		print("GY65 Altitude:\t{0} m".format(Altitude))
-		display_text(2,11, "{0} hPa".format(Pressure))	
+		display_text(0,22, "{0} hPa".format(Pressure))	
 		
 		Luminosity = readLuminosity()
 		print("GY30 Light:\t{0} lx".format(Luminosity))
-		display_text(2,20, "{:.2f} lx".format(Luminosity))	
+		display_text(65,22, "{:.2f} lx".format(Luminosity))	
 
-		
-		# Fields: Indoor Temp, Humidity, Outdoor Temp, Pressure, Altitude, Indoor Temp2, Luminosity, Motion
-		params=(InTemp, Humi, OutTemp, Pressure, Altitude, InTemp2, Luminosity, "")
-		publishThingspeak(thingspeak_key, params)
-		
+		if len(sys.argv) == 1:
+			# Fields: Indoor Temp, Humidity, Outdoor Temp, Pressure, Altitude, Indoor Temp2, Luminosity, Motion
+			params=(InTemp, Humi, OutTemp, Pressure, Altitude, InTemp2, Luminosity, "")
+			publishThingspeak(thingspeak_key, params)
 
 	except:
 		print "Some error ocurred"
